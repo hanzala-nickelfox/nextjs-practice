f532798f6884c0b5e13038df12118524f26b1475
mayur-nickelfox <mayur+1@nickelfox.com>
2023-04-12 17:44:28 +0530

    fix(cookies): dynamic cookie as per env

--- src/constants/cookieKeys.js
+++ src/constants/cookieKeys.js
@@ -1,9 +1,9 @@
 import { Dates } from "@local/helpers/app-dates/dates"
 
 export const CookieKeys = {
-  Auth: "Auth-Token",
-  API_TOKEN: "api-key",
-  REFRESH_TOKEN: "Refresh-Token"
+  Auth: `${process.env.NEXT_PUBLIC_ENV}:Auth-Token`,
+  API_TOKEN: `${process.env.NEXT_PUBLIC_ENV}:api-key`,
+  REFRESH_TOKEN: `${process.env.NEXT_PUBLIC_ENV}:Refresh-Token`
 }
 
 console.log(process.env.APP_ENV, process.env.APP_ENV)
